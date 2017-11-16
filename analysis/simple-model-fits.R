01efde963e8f172272e3179fa6d0a63503290265
Lucy Li <luli@hsph.harvard.edu>
2017-11-15 19:40:02 -0500

    simple model fit

--- analysis/simple-model-fits.R
+++ analysis/simple-model-fits.R
@@ -1,11 +1,24 @@
 aggregate.by.week <- function (x) {
   require(magrittr)
+  require(lubridate)
   if ("Governorate" %in% names(x)) {
+    x <- split(x, week(x$Date)) %>%
+      lapply(function (y) {
+        notin <- !(unique(x$Governorate) %in% y$Governorate)
+        if (any(notin)) {
+          y <- rbind(y, y[1:sum(notin), ])
+          y[nrow(y)-(sum(notin):1)+1, "Governorate"] <- unique(x$Governorate)[notin]
+          y[nrow(y)-(sum(notin):1)+1, "NewCases"] <- 0
+        }
+        y
+      }) %>%
+      do.call(what=rbind)
     groupby <- group_by(x, aggDate=week(Date), Governorate)
   } else {
     groupby <- group_by(x, aggDate=week(Date))
   }
-  output <- groupby %>% summarize(NewCases=sum(NewCases), minDate=min(Date))
+  output <- groupby %>% summarize(NewCases=sum(NewCases)) %>% 
+    mutate(minDate=floor_date(as.Date("2016-12-31")+aggDate*7, unit="weeks"))
 }
 
 plot.weekly.ts <- function (x, xlab="Date", ylab="New Cases Per Week", 
@@ -18,11 +31,10 @@ plot.weekly.ts <- function (x, xlab="Date", ylab="New Cases Per Week",
     xlab(xlab) + ylab(ylab) + ggtitle(title)
 }
 
-combine.spatiotemporal <- function (spatial, temporal.df, time) {
+combine.spatiotemporal <- function (spatial, temporal.df) {
   output <- temporal.df %>%
-    filter(aggDate==time) %>%
-    mutate(id=as.character(match(Governorate, spatial$info$admin1Name_en))) %>%
-    inner_join(spatial$shp, .)
+    mutate(id=as.character(as.numeric(match(Governorate, spatial$shp$name_en)-1))) %>%
+    full_join(spatial$shpdf, .)
   output$NewCases[is.na(output$NewCases)] <- 0
   output
 }
@@ -33,17 +45,25 @@ get.week.string <- function (input.date) {
     `+`(c(0, 6)) %>% 
     format("%b %d") %>% 
     paste(., collapse=" to ") %>% 
-    paste0("Week of ", ., " 2017")
+    paste0(., " 2017")
 }
 
 plot.spatiotemporal <- function (x, cols=c("#FFF5EE", "#CD3333")) {
   require(ggplot2)
-  ggplot(x, aes(x = long, y = lat, group = group, fill=NewCases, frame=aggDate)) + 
-    theme_classic() +
+  title.vec <- sapply(unique(x$minDate), get.week.string)
+  positions <- x %>% 
+    split(., .$id) %>% 
+    lapply(function (x) c(x=mean(range(x$long)), y=mean(range(x$lat)), id=as.character(x$Governorate[1]))) %>% 
+    do.call(what=rbind)
+  x %<>% filter(!is.na(aggDate))
+  ggplot(x, aes(x = long, y = lat, group = group, fill=NewCases, 
+                frame=paste0("Week ", aggDate, ": ", title.vec[aggDate-min(aggDate, na.rm=TRUE)+1]))) + 
+    theme_bw() +
     geom_polygon(color = 'gray') +
+    annotate("text", x=as.numeric(positions[, 1]), y=as.numeric(positions[, 2]), label=positions[, 3]) +
     theme(axis.text=element_blank(), 
           axis.title=element_blank(),
-          axis.ticks=element_blank()) +
-    scale_fill_gradient(low=cols[1], high=cols[2]) +
-    ggtitle(get.week.string(x$minDate[1]))
+          axis.ticks=element_blank(),
+          title=element_text(hjust=0.5)) +
+    scale_fill_gradient("New Cases", low=cols[1], high=cols[2])
 }
